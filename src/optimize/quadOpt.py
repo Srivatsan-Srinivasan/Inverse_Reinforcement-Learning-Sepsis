b0559b5d6eac1598ed9a3c6edf7fbd8cfca26f58
Srivatsan-Srinivasan <31608551+Srivatsan-Srinivasan@users.noreply.github.com>
2017-10-31 22:27:26 -0400

    Optimizer commit

---
+++ src/optimize/quadOpt.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 19:29:40 2017
+
+@author: SrivatsanPC
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+import pdb
+
+class quadOpt():
+    def __init__(self,epsilon = 0.01):
+        self.SVMKernelType = "linear"
+        self.mu_list = []
+        self.epsilon = epsilon
+        self.SVMpenalty = 1000
+    
+    def transDataforSVM(self, target_mu, cur_mu ):
+        self.mu_list.append(cur_mu) 
+        #pdb.set_trace()
+        X_candidate = self.mu_list
+        X_target = target_mu        
+        y = [0 for i in range(len(X_candidate))]
+        X_candidate.append(X_target)
+        self.mu_list.append(X_target)
+        y.append(1)
+        return X_candidate,y
+    
+    def optOutput(self, target_mu, cur_mu, norm_weights = True):
+        clf = svm.SVC(kernel= self.SVMKernelType, C= self.SVMpenalty)
+        
+        X,y = self.transDataforSVM(target_mu,cur_mu)
+       
+        clf.fit(X,y)
+        margin = 1 / np.sqrt(np.sum(clf.coef_ ** 2))
+        converged = (margin <= self.epsilon)
+        weights = clf.coef_
+        pdb.set_trace()
+        if norm_weights:
+            weight_norm = sum([abs(w) for w in weights.flatten()])
+            weights = weights.flatten()/weight_norm
+        return weights, converged
+      
+        
