139cd698f34f8635ec50dc1b514d19a7f029f472
dhfromkorea <dhfromkorea@gmail.com>
2017-11-05 04:50:18 -0500

    fix svm solver

--- src/optimize/quad_opt.py
+++ src/optimize/quad_opt.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
+import seaborn as sns
+sns.set(style='dark')
+from scipy.optimize.minpack import curve_fit
 
 class QuadOpt():
     def __init__(self,epsilon = 0.01):
@@ -8,52 +11,58 @@ class QuadOpt():
         self.mu_list = []
         self.target = None
         self.epsilon = epsilon
-        self.SVMpenalty = 0.5
-        plt.ion()
-        self.is_tmu = True
-        fig, ax = plt.subplots()
-        self.fig = fig
-        self.ax = ax
-        self.plot = ax.scatter([], [])
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
+        self.SVMpenalty = 1000.0
+        self.margins = []
+        self.counter = 0
 
     def transform_data(self, target_mu, cur_mu ):
         self.mu_list.append(cur_mu)
         X = self.mu_list + [target_mu]
+        X = np.array(X)
         y = np.zeros(len(X)).astype(np.int)
         y[-1] = 1
-
-        arr = self.plot.get_offsets()
-        if self.is_tmu:
-            array = np.append(arr, cur_mu)
-            self.ax.scatter(target_mu[0], target_mu[1], marker='*', color='y')
-            self.is_tmu = False
-        else:
-            array = np.append(arr, cur_mu)
-        self.plot.set_offsets(array)
-        # update x and ylim to show all points:
-        self.ax.set_xlim(array.min() - 10, array.max() + 10)
-        self.ax.set_ylim(array.min() - 10, array.max() + 10)
-
-        # update the figure
-        self.fig.canvas.draw()
-        
+        y = np.array(y)
         return X, y
     
     def optimize(self, target_mu, cur_mu, norm_weights=True):
-        #import pdb;pdb.set_trace()
         clf = svm.SVC(kernel=self.SVMKernelType, C=self.SVMpenalty)
         X,y = self.transform_data(target_mu,cur_mu)
        
         clf.fit(X,y)
         margin = 1 / np.sqrt(np.sum(clf.coef_ ** 2))
         converged = (margin <= self.epsilon)
-        weights = clf.coef_
-        import pdb;pdb.set_trace()
+        weights = clf.coef_[0]
+        bias = clf.intercept_
+
+        self.margins.append(margin)
+        self.counter += 1
+
+        if self.counter % 10 == 0:
+            fig, (ax1, ax2) = plt.subplots(1, 2)
+            # plot mu vectors
+            x1 = np.linspace(X[:,0].min(), X[:,0].max(), 100)
+            x2 = -weights[0]/weights[1]*x1 - bias/weights[1]
+            c = range(len(y) - 1)
+            cm = plt.cm.get_cmap('Purples')
+            ax1.scatter(x=X[:-1, 0], y=X[:-1, 1], c=c, cmap=cm)
+            ax1.scatter(x=X[-1, 0], y=X[-1, 1], marker='*')
+            ax1.plot(x1, x2, label='hyperplane')
+            # plot margins
+            # curve fitting
+            import pdb;pdb.set_trace()
+            exp_decay = lambda x, A, t, y0: A * np.exp(x * t) + y0
+            xx = range(self.counter)
+            params, cov = curve_fit(exp_decay, xx, self.margins, maxfev=10000)
+            yy = exp_decay(xx, *params)
+            ax2.plot(xx, yy, label='smooth')
+            ax2.plot(self.margins, label='margins')
+            plt.legend()
+            plt.show()
+        
         if norm_weights:
             weight_norm = np.linalg.norm(weights, 2)
-            weights = weights.flatten()/weight_norm
+            weights = weights/weight_norm
         else:
-            weights = weights.flatten()
+            weights = weights
+        print(weights)
         return weights, converged, margin
