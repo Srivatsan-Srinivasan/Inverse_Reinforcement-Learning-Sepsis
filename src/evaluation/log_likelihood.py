3f8b6bfb580a20632b8507fbc56cf7e3727e9d85
unknown <vatsan09@gmail.com>
2017-11-23 22:22:16 -0500

    Getting better evaluation functions

--- src/evaluation/log_likelihood.py
+++ src/evaluation/log_likelihood.py
@@ -10,20 +10,25 @@ from utils.utils import load_data, extract_trajectories
 from constants import NUM_STATES, NUM_ACTIONS, TERMINAL_STATE_ALIVE, TERMINAL_STATE_DEAD, NUM_PURE_STATES
 import numpy as np
 import math
+import pdb
 
 def test_code():
     print("Hello")
     
 #opt_policy_learned should be a stochastic policy for each state, essentially a 750*25 array.
 #phy_policy is single point estimate - one for each state - 750*1 array. phi_policy_stochastic is again 750*25 array.
-def get_log_likelihood(df_cleansed,opt_policy_learned,phy_policy,phy_policy_stochastic,num_actions=25,no_int_id=0):
-    trajs = extract_trajectories(df_cleansed, NUM_PURE_STATES)
+def get_log_likelihood(df_cleansed, opt_policy_learned, phy_policy, phy_policy_stochastic, num_states = NUM_PURE_STATES, num_actions=25, 
+                       no_int_id=0, restrict_num = False, avg = False):
+    trajs = extract_trajectories(df_cleansed, num_states )
     uni_trajs = np.unique(trajs[:, 0])
     LL = {}
-    for traj_id in uni_trajs():
+    #pdb.set_trace()
+    if restrict_num:
+        uni_trajs = uni_trajs[:100]
+    for traj_id in uni_trajs:
         path_taken = trajs[np.array([i[0]==traj_id for i in trajs])]
         #id,s,a,r,s'.Extract state,action
-        path_taken_s_a = [path_taken[1:3]]
+        path_taken_s_a = [p[1:3] for p in path_taken]
         target_acts = []
         target_acts_stoch = []
         candidate_acts = []
@@ -32,23 +37,26 @@ def get_log_likelihood(df_cleansed,opt_policy_learned,phy_policy,phy_policy_stoc
             target_acts.append(phy_policy[elem[0]])
             target_acts_stoch.append(phy_policy_stochastic[elem[0]])
             candidate_acts.append(opt_policy_learned[elem[0]])
-        LL[traj_id] = {"LL_IRL" : eu.log_likelihood(candidate_acts,target_acts),
-                       "LL_random" : math.log((1/num_actions))* len(target_acts),
-                       "LL_noint" : eu.log_likelihood_no_act(target_acts_stoch,no_int_id),
+        LL[traj_id] = {"IRL" : eu.log_likelihood(candidate_acts,target_acts,avg = avg),
+                       "random" : avg * math.log((1/num_actions)) + (1-avg)* math.log((1/num_actions)) * len(target_acts),
+                       "no_int" : eu.log_likelihood_no_act(target_acts_stoch,no_int_id,avg = avg),
                        "length" : len(target_acts)}
     return LL
 
 def get_KL_divergence(phy_policy_stochastic, opt_policy_learned, significant_states = range(750)):
     no_int_policy = eu.gen_single_action_probability()
     random_policy = eu.gen_random_probability()
-    
+    vaso_only_random_policy = eu.gen_random_probability(random_action_range= range(5))
+    iv_only_random_policy = eu.gen_random_probability(random_action_range= np.array(range(5)) * 5 )
     KL = {}
     for state in significant_states:
         phy_policy = phy_policy_stochastic[state]
         opt_policy = opt_policy_learned[state]
         KL[state] = {"IRL" : eu.KL_divergence(phy_policy,opt_policy),
                      "random" : eu.KL_divergence(phy_policy,random_policy),
-                     "no_int_policy" : eu.KL_divergence(phy_policy,no_int_policy)                   
+                     "no_int_policy" : eu.KL_divergence(phy_policy,no_int_policy) ,                  
+                     "vaso_only_random" : eu.KL_divergence(phy_policy,vaso_only_random_policy),
+                     "iv_only_random" : eu.KL_divergence(phy_policy,iv_only_random_policy)
                     }
     return KL
         
--- src/utils/evaluation_utils.py
+++ src/utils/evaluation_utils.py
@@ -8,6 +8,11 @@ from sklearn.metrics import mutual_info_score as mis
 from scipy.stats import entropy
 import numpy as np
 import math
+import pdb
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import mean
+import pandas as pd
 
 #p,q are distributions - p is base distribution and q is approximate
 def KL_divergence(p,q):
@@ -23,32 +28,47 @@ def single_entropy(p):
 def mis_score(p,q):
     return mis(p,q)
 
-def gen_random_probability(num_actions = 25):
-    out = np.array([1/num_actions for i in range(num_actions)])
+def gen_random_probability(num_actions = 25, epsilon = 0.001, random_action_range = list(range(25))):
+    def assign(x,random_action_range):
+        if x in random_action_range:
+            return 1/len(random_action_range)
+        else:
+            return epsilon
+    #pdb.set_trace()           
+    out = [assign(x,random_action_range) for x in range(num_actions)]
+    out = np.array(out)/np.round(sum(out),3)
+  
+    assert(abs(sum(out)- 1) < 0.01 and len(out) == num_actions)
+    return np.array(out)
+
+def gen_single_action_probability(num_actions = 25, action_id = 0,epsilon = 0.001):
+    out = np.ones(num_actions) * epsilon
+    out[action_id] = 1 - (num_actions-1) * epsilon
     assert(sum(out) == 1 and len(out) == num_actions)
     return out
 
-def gen_single_action_probability(num_actions = 25, action_id = 0):
-    out = np.zeros(num_actions)
-    out[action_id] = 1
-    assert(sum(out) == 1 and len(out) == num_actions)
-    return out
-
-def log_likelihood(prob_actions, target_actions):
+def log_likelihood(prob_actions, target_actions,avg = False ):
     out = 0
     #SANITY checks.
     assert(len(prob_actions) == len(target_actions))
-    assert(len(prob_actions[0]) == 25)
+    #pdb.set_trace()
+    #assert(len(prob_actions[0]) == 25)
     
     for i in range(len(target_actions)):
         target_act = target_actions[i]
         candidate_act_prob = prob_actions[i]
-        out += math.log(candidate_act_prob[target_act])
+        #pdb.set_trace()
+        out += math.log(candidate_act_prob[int(target_act)])
+    if avg : 
+        return out / len(target_actions) 
+    
     return out
 
-def log_likelihood_no_act(target_acts_stoch, no_int_id):
-    sum([math.log(i[no_int_id] + 1e-3) for i in target_acts_stoch ])
-
+def log_likelihood_no_act(target_acts_stoch, no_int_id,avg = False):
+    if avg:
+        return(np.mean([math.log(i[no_int_id] + 1e-3) for i in target_acts_stoch ]))
+        
+    return(sum([math.log(i[no_int_id] + 1e-3) for i in target_acts_stoch ]))
 
 def test_code():
     a = [0.1,0.2,0.3,0.4]
@@ -66,9 +86,79 @@ def test_code():
     print("CE(",a,",",c,") : ",cross_entropy(a,c))
     print("CE(",a,",",d,") : ",cross_entropy(a,d))
     print("CE(",a,",",e,") : ",cross_entropy(a,e))
+    
+    assert(np.array_equal(gen_random_probability(), np.array([0.04 for i in range(25)])))
+    assert(np.allclose(gen_random_probability(random_action_range= range(5)), 
+                          np.concatenate([np.array([0.2 for i in range(5)]),np.zeros(20)*0.001]),
+                       atol = 0.01  ))
  
+def sumzip(*items):
+    return [sum(values) for values in zip(*items)]
 
+def plot_KL(KL, show = True, test = True):
+    KL_IRL = []
+    KL_random = []
+    KL_vaso_random = []
+    KL_iv_random = []
+    KL_no_int = []
+    keys = list(KL.keys())
+    for state in keys:
+        KL_IRL.append(KL[state]["IRL"])
+        KL_random.append(KL[state]["random"])
+        KL_no_int.append(KL[state]["no_int_policy"])
+        KL_vaso_random.append(KL[state]["vaso_only_random"])
+        KL_iv_random.append(KL[state]["iv_only_random"])
+        
+    plt.bar(keys,KL_IRL,color = 'b',label="IRL")
+    plt.bar(keys,KL_random,bottom = sumzip(KL_IRL), color = 'r', label = "Random")
+    plt.bar(keys,KL_no_int,bottom = sumzip(KL_IRL,KL_random), color = 'y', label = "No intervnetion")
+    plt.bar(keys,KL_vaso_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int), color = 'g', label = "Vaso Only Random")
+    plt.bar(keys,KL_iv_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int,KL_vaso_random), color = 'm', label = "IV Only Random" )
+    test_str = "train"
+    if test:
+        test_str = "test"
+    plt.title("KL divergences with respect to physician policy in " + test_str + " data" )
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    if show:
+        plt.show()
+    else:
+        return plt
 
+def plot_avg_LL(LL,show = True, test = True):
+    keys = list(LL.keys())
+    key_len = len(keys)
+    
+    NLL_IRL = []
+    NLL_random = []
+    NLL_no_int = []
+
+    for p in range(key_len):
+        NLL_IRL.append(-1 * LL[keys[p]]["IRL"])
+        NLL_random.append(-1 * LL[keys[p]]["random"])
+        NLL_no_int.append(-1 * LL[keys[p]]["no_int"])
+    
+    pdb.set_trace()
+#    plt.bar("IRL", np.mean(LL_IRL), color = 'b', label="IRL")
+#    plt.bar("Random", np.mean(LL_random), color = 'g', label = "Random")
+#    plt.bar("No intervention", np.mean(LL_no_int), color = 'r', label = "No Intervention")
+#    test_str = "train"
+#    if test:
+#        test_str = "test"
+#    plt.title("Average log-likelihood for different policies to predict physician action in " + test_str + " data")
+#    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+#    if show:
+#        plt.show()
+#    else:
+#        return plt
+    pdb.set_trace()
+    data_NLL = pd.DataFrame(np.transpose(np.matrix([NLL_IRL,NLL_random,NLL_no_int])))
+    data_NLL.columns = ["IRL", "Random", "No_Intervention"]
+    plot = sns.barplot(data=data_NLL, estimator=mean)
+    plot.title("Plot showing average negative LL")
+    plot.show()
+    pdb.set_trace()
+    assert(True)
+        
 test_code()
     
     
