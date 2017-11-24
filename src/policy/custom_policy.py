db4499e1db77b72515aa998ec6513fc6b1c965db
dh <dhfromkorea@gmail.com>
2017-11-23 22:20:56 -0500

    update extract pysician policy
    now can handle both stochastic and greedy

--- src/policy/custom_policy.py
+++ src/policy/custom_policy.py
@@ -4,31 +4,25 @@ from constants import *
 from policy.policy import GreedyPolicy, StochasticPolicy
 
 
-def get_physician_policy(trajectories):
+def get_physician_policy(trajectories, is_stochastic=False):
     '''
     get a clincian policy (greedy to their mode actions)
     '''
     # when tie, smallest index returned
-    cols = ['s', 'a']
-    df = pd.DataFrame(trajectories[:,1:3], columns=cols)
+    df = pd.DataFrame(trajectories[:,1:3], columns=['s', 'a'])
+    trajectories = trajectories.astype(np.int)
     groups_s = df.groupby(['s'])
-    mode_actions = groups_s.agg(lambda x: x.value_counts().index[0])
-    Q = np.eye(NUM_ACTIONS, dtype=np.float)[mode_actions]
-    Q = Q.reshape(NUM_PURE_STATES, NUM_ACTIONS)
-    pi = GreedyPolicy(NUM_PURE_STATES, NUM_ACTIONS, Q)
+    if is_stochastic:
+        Q = np.zeros((NUM_STATES, NUM_ACTIONS))
+        action_dist = df.groupby(['s'])['a'].apply(lambda x : x.value_counts())
+        idx = np.array([[x[0], x[1]] for x in action_dist.index.values])
+        Q[idx[:,0], idx[:,1]] = action_dist
+        pi = StochasticPolicy(NUM_STATES, NUM_ACTIONS, Q)
+    else:
+        mode_actions = df.groupby(['s']).agg(lambda x: x.value_counts().index[0])['a']
+        Q = np.eye(NUM_ACTIONS, dtype=np.float)[mode_actions]
+        Q = np.vstack([Q, np.zeros((NUM_TERMINAL_STATES, NUM_ACTIONS))])
+        pi = GreedyPolicy(NUM_STATES, NUM_ACTIONS, Q)
     return pi
 
-
-def make_physician_stochastic_policy(trajectories, num_states, num_actions):
-    '''
-    another version that learns a stochastic policy
-    based on the histogram of different actions given state
-    TODO: not tested yet
-    '''
-    cols = ['s', 'a']
-    df = pd.DataFrame(trajectories[:,1:3], columns=cols)
-    groups_s = df.groupby(['s'])
-    import pdb;pdb.set_trace()
-    pi = StochasticPolicy(NUM_PURE_STATES, NUM_ACTIONS, Q_table)
-    return pi
 
