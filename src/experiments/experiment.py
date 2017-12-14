fix kl and ll

src/experiments/experiment.py
@@ -131,20 +131,11 @@ class ExperimentManager():
         # experiments
         self.experiments = []
 
-        df_vaso_sd, df_iv_sd = get_sd_away_bins(self.df)
-        self.df_vaso_sd = df_vaso_sd
-        self.df_iv_sd = df_iv_sd
+        self.df_vaso_sd, self.df_iv_sd = get_sd_away_bins(self.df)
+        self.pi_phy_vaso, self.pi_phy_iv = self._decompose_phy_action(self.df)
+        self.pi_mdp_vaso_probs, self.pi_mdp_iv_probs = \
+            self._decompose_mdp_action_probs(self.df, self.pi_expert_mdp_s)
 
-
-        mdp_vaso_probs, mdp_iv_probs, irl_vaso_probs, irl_iv_probs = \
-            self._prepare_deviation_plots(self.df,
-                                          self.pi_expert_mdp_s,
-                                          pi_irl_s)
-
-        self.pi_mdp_vaso_probs = mdp_vaso_probs
-        self.pi_mdp_iv_probs = mdp_iv_probs
-        self.pi_irl_vaso_probs = irl_vaso_probs
-        self.pi_irl_iv_probs = irl_iv_probs
 
 
     def save_experiment(self, res, exp):
@@ -205,26 +196,30 @@ class ExperimentManager():
                                         NUM_ACTIONS,
                                         res['approx_expert_Q'])
 
+            pi_irl_vaso_probs, pi_irl_iv_probs =\
+                    self._decompose_irl_action_probs(self.df, pi_irl_s)
 
 
             plot_deviation_from_experts(self.df_vaso_sd,
                                         self.pi_phy_vaso,
                                         self.pi_mdp_vaso_probs,
-                                        self.pi_irl_vaso_probs,
+                                        pi_irl_vaso_probs,
                                         self.img_path,
-                                        (exp.experiment_id + '_vaso'),
+                                        exp.experiment_id,
                                         exp.num_trials,
-                                        exp.num_iterations)
+                                        exp.num_iterations,
+                                        action_name='vaso')
 
 
             plot_deviation_from_experts(self.df_iv_sd,
                                         self.pi_phy_iv,
                                         self.pi_mdp_iv_probs,
-                                        self.pi_irl_iv_probs,
+                                        pi_irl_iv_probs,
                                         self.img_path,
-                                        (exp.experiment_id + '_iv'),
+                                        exp.experiment_id,
                                         exp.num_trials,
-                                        exp.num_iterations)
+                                        exp.num_iterations,
+                                        action_name='iv')
             # kl and loglikelihood
             LL = lh.get_log_likelihood(df,
                                        pi_irl_s,
@@ -251,7 +246,8 @@ class ExperimentManager():
                        iter_num=exp.num_iterations,
                        trial_num=exp.num_trials)
 
-    def _prepare_deviation_plots(self, df, pi_mdp, pi_irl):
+    def _decompose_phy_action(self, df):
+
         path_vaso = TRAJECTORIES_PCA_VASO_FILEPATH if self.use_pca else TRAJECTORIES_VASO_FILEPATH
         #path_vaso_sd_l = TRAJECTORIES_PCA_VASO_SD_LEFT_FILEPATH if self.use_pca else TRAJECTORIES_VASO_SD_LEFT_FILEPATH
         #path_vaso_sd_r = TRAJECTORIES_PCA_VASO_SD_RIGHT_FILEPATH if self.use_pca else TRAJECTORIES_VASO_SD_RIGHT_FILEPATH
@@ -266,20 +262,21 @@ class ExperimentManager():
         #traj_iv_sd_l = extract_trajectories(self.df, NUM_PURE_STATES, path_iv_sd_l, 'action_iv_sd_left')
         #traj_iv_sd_r = extract_trajectories(self.df, NUM_PURE_STATES, path_iv_sd_r, 'action_iv_sd_right')
         num_bins = 5
-        self.pi_phy_vaso = get_physician_policy(traj_vaso, num_actions=num_bins, is_stochastic=True)
+        pi_phy_vaso = get_physician_policy(traj_vaso, num_actions=num_bins, is_stochastic=True)
         #self.pi_phy_vaso_sd_l = get_physician_policy(traj_vaso_sd_l, num_actions=num_bins, is_stochastic=True)
         #self.pi_phy_vaso_sd_r = get_physician_policy(traj_vaso_sd_r, num_actions=num_bins, is_stochastic=True)
-        self.pi_phy_iv = get_physician_policy(traj_iv, num_actions=num_bins, is_stochastic=True)
+        pi_phy_iv = get_physician_policy(traj_iv, num_actions=num_bins, is_stochastic=True)
         #self.pi_phy_iv_sd_l = get_physician_policy(traj_iv_sd_l, num_actions=num_bins, is_stochastic=True)
         #self.pi_phy_iv_sd_r = get_physician_policy(traj_iv_sd_r, num_actions=num_bins, is_stochastic=True)
+        return pi_phy_vaso, pi_phy_iv
 
+    def _decompose_mdp_action_probs(self, df, pi_mdp):
         num_bins = 5
         mdp_probs = pi_mdp.query_Q_probs()
         mdp_vaso_probs = np.zeros((NUM_PURE_STATES, num_bins))
         mdp_iv_probs = np.zeros((NUM_PURE_STATES, num_bins))
-        irl_probs = pi_irl.query_Q_probs()
-        irl_vaso_probs = np.zeros((NUM_PURE_STATES, num_bins))
-        irl_iv_probs = np.zeros((NUM_PURE_STATES, num_bins))
+
+
         vaso_bins = np.arange(25) // num_bins
         iv_bins = np.arange(25) % num_bins
 
@@ -287,17 +284,35 @@ class ExperimentManager():
             # marginalize out iv and vaso
             mdp_vaso_probs[:, bin_i] = np.sum(mdp_probs[:, np.flatnonzero(vaso_bins == bin_i)], axis=1)
             mdp_iv_probs[:, bin_i] = np.sum(mdp_probs[:, np.flatnonzero(iv_bins == bin_i)], axis=1)
-            irl_vaso_probs[:, bin_i] = np.sum(irl_probs[:, np.flatnonzero(vaso_bins == bin_i)], axis=1)
-            irl_iv_probs[:, bin_i] = np.sum(irl_probs[:, np.flatnonzero(iv_bins == bin_i)], axis=1)
 
         assert np.isclose(np.sum(mdp_vaso_probs), NUM_PURE_STATES)
         assert np.isclose(np.sum(mdp_iv_probs), NUM_PURE_STATES)
+
+        return mdp_vaso_probs, mdp_iv_probs
+
+
+    def _decompose_irl_action_probs(self, df, pi_irl):
+        num_bins = 5
+
+        irl_probs = pi_irl.query_Q_probs()
+        irl_vaso_probs = np.zeros((NUM_PURE_STATES, num_bins))
+        irl_iv_probs = np.zeros((NUM_PURE_STATES, num_bins))
+
+        vaso_bins = np.arange(25) // num_bins
+        iv_bins = np.arange(25) % num_bins
+
+        for bin_i in range(num_bins):
+            # marginalize out iv and vaso
+            irl_vaso_probs[:, bin_i] = np.sum(irl_probs[:, np.flatnonzero(vaso_bins == bin_i)], axis=1)
+            irl_iv_probs[:, bin_i] = np.sum(irl_probs[:, np.flatnonzero(iv_bins == bin_i)], axis=1)
+
         assert np.isclose(np.sum(irl_vaso_probs), NUM_PURE_STATES)
         assert np.isclose(np.sum(irl_iv_probs), NUM_PURE_STATES)
 
-        return mdp_vaso_probs, mdp_iv_probs, irl_vaso_probs, irl_iv_probs
+        return irl_vaso_probs, irl_iv_probs
 
 
+        pass
     def set_experiment(self, exp):
         exp.transition_matrix_train = self.transition_matrix_train
         exp.transition_matrix = self.transition_matrix

src/utils/evaluation_utils.py
@@ -109,18 +109,21 @@ def plot_KL(KL, save_path, plot_suffix, trial_num, iter_num, show = True, test =
         KL_no_int.append(KL[state]["no_int_policy"])
         KL_vaso_random.append(KL[state]["vaso_only_random"])
         KL_iv_random.append(KL[state]["iv_only_random"])
+
     fig = plt.figure(figsize=(10, 10))
-    #data_NLL = pd.DataFrame(np.transpose(np.matrix([KL_IRL,NLL_random,NLL_no_int])))
-    #data_NLL.columns = ["IRL", "Random", "No_Intervention", "Vaso Only Random", "IV Only Random"]
-    #sns.barplot(data=data_NLL, estimator=mean)
-    plt.bar(keys,KL_IRL,color = 'b',label="IRL")
-    plt.bar(keys,KL_random,bottom = sumzip(KL_IRL), color = 'r', label = "Random")
-    plt.bar(keys,KL_no_int,bottom = sumzip(KL_IRL,KL_random), color = 'y', label = "No intervnetion")
-    plt.bar(keys,KL_vaso_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int), color = 'g', label = "Vaso Only Random")
-    plt.bar(keys,KL_iv_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int,KL_vaso_random), color = 'm', label = "IV Only Random" )
-    test_str = "train"
-    if test:
-        test_str = "test"
+    data = np.transpose(np.matrix([KL_IRL, KL_random, KL_no_int, KL_vaso_random, KL_iv_random]))
+    data_KL = pd.DataFrame(data)
+    data_KL.columns = ["IRL", "Random", "No_Intervention", "Vaso Only Random", "IV Only Random"]
+    sns.barplot(data=data_KL, estimator=mean)
+
+    #plt.bar(keys,KL_IRL,color = 'b',label="IRL")
+    #plt.bar(keys,KL_random,bottom = sumzip(KL_IRL), color = 'r', label = "Random")
+    #plt.bar(keys,KL_no_int,bottom = sumzip(KL_IRL,KL_random), color = 'y', label = "No intervnetion")
+    #plt.bar(keys,KL_vaso_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int), color = 'g', label = "Vaso Only Random")
+    #plt.bar(keys,KL_iv_random, bottom = sumzip(KL_IRL,KL_random,KL_no_int,KL_vaso_random), color = 'm', label = "IV Only Random" )
+    #test_str = "train"
+    #if test:
+    #    test_str = "test"
     #plt.title("KL divergences with respect to physician policy in " + test_str + " data" )
     #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.legend(loc='best', fontsize=FONT_SIZE)
@@ -149,10 +152,16 @@ def plot_avg_LL(LL, save_path, plot_suffix, trial_num, iter_num, show = True, te
 
     fig = plt.figure(figsize=(10, 10))
     data_NLL = pd.DataFrame(np.transpose(np.matrix([NLL_IRL,NLL_random,NLL_no_int])))
-    data_NLL.columns = ["IRL", "Random", "No_Intervention"]
+    columns = [None]*3
+    d = np.argsort(data_NLL.mean().tolist())
+    columns[d[0]] = 'IRL'
+    columns[d[1]] = 'No_intervention'
+    columns[d[2]] = 'Random'
+    data_NLL.columns = columns
+    # @refactor @hack
     sns.barplot(data=data_NLL, estimator=mean)
     plt.xlabel('Policies', fontsize=FONT_SIZE)
-    plt.ylabel('Log Likelihood', fontsize=FONT_SIZE)
+    plt.ylabel('Negative Log Likelihood', fontsize=FONT_SIZE)
     #plot.title("Plot showing average negative LL")
     if show:
         plt.show()

src/utils/plot.py
@@ -112,7 +112,8 @@ def plot_deviation_from_experts(sd,
                                 save_path,
                                 plot_prefix,
                                 num_trials,
-                                num_iterations):
+                                num_iterations,
+                                action_name):
     '''
     compare clinician, mdp
     compare clinician, irl
@@ -120,7 +121,7 @@ def plot_deviation_from_experts(sd,
     # hyperparameters
     # some preprocessing, cap bin jump by one bin only
     # e.g. for iv, std is too high, it often jumps by two bins
-    thresholds = np.array([0.05, 0.15, 0.25])
+    thresholds = np.array([0.10, 0.20, 0.30])
     pi_phy_probs = pi_phy.query_Q_probs()
     violations = np.zeros((2, len(thresholds), NUM_PURE_STATES))
     num_bins = 5
@@ -147,13 +148,13 @@ def plot_deviation_from_experts(sd,
     colors = [['firebrick', 'red', 'darksalmon'], ['blue', 'royalblue', 'navy']]
     for i, _ in enumerate([pi_mdp_probs, pi_irl_probs]):
         for j, th in enumerate(thresholds):
-            plt.plot(violations[i, j, :], linestyle=linestyles[i], c=colors[i][j], label='{}, threshold={}%'.format(desc[i], 100 * th))
-    quartiles = np.arange(5)/4.0
+            plt.plot(violations[i, j, :], linestyle=linestyles[i], c=colors[i][j], label='{}_{} (> {}%)'.format(action_name, desc[i], int(100 * th)))
+    quartiles = np.arange(5, dtype=np.int)/4
     plt.xticks(np.around(750 * quartiles), 100 * quartiles)
     plt.legend(loc='best')
     plt.xlabel('State Quantiles (%)', size=FONT_SIZE)
     plt.ylabel('Proportion of Violations (%)', size=FONT_SIZE)
-    fig.savefig('{}{}_violations_t{}xi{}'.format(save_path, plot_prefix, num_trials, num_iterations), ppi=300, bbox_inches='tight')
+    fig.savefig('{}{}_{}_violations_t{}xi{}'.format(save_path, action_name, plot_prefix, num_trials, num_iterations), ppi=300, bbox_inches='tight')
     plt.close()
 
 #def plot_deviation_from_experts(pi_phy,